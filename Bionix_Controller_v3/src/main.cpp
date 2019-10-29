ef2a6f4a391d18772de223b063417aee6f074fa5
Riya Kadakia <riyakadakia@riyas-air.lan>
2019-10-28 19:50:06 -0700

    initial checkin of bionix_controller code

---
+++ Bionix_Controller_v3/src/main.cpp
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       riyakadakia                                               */
+/*    Created:      Mon Oct 28 2019                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// Drivetrain           drivetrain    2, 10           
+// ArmMotor             motor         6               
+// StackerMotor         motor         7               
+// LeftClawMotor        motor         8               
+// RightClawMotor       motor         3               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
