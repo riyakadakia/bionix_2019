ed02b88500c475f23aa38158e8d176bcc5cb5827
Viral Kadakia <vkadakia@Virals-MacBook-Pro.local>
2019-12-12 13:38:50 -0600

    added new functions to turn the motor, start and stop spinning claws and added more comments

--- Auton_v1/src/main.cpp
+++ Auton_v1/src/main.cpp
@@ -35,29 +35,68 @@
 
 using namespace vex;
 
-double convertInchesIntoTicks(float inches) {
+const float WHEEL_DIAMETER = 4.125; // inches
+const float WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * 3.1416;
+const int TICKS_PER_REVOLUTION_36 = 1800; // number of ticks per revolution for 36:1 gear ratio
+const int TICKS_PER_REVOLUTION_18 = 900; // number of ticks per revolution for 18:1 gear ratio
+const float TURNING_DIAMETER = 21.21; // turning diameter or diagonal distance between the wheels
+
+/**
+  convertInchesIntoTicks_18 converts 'inches' into the appropriate number of motor ticks
+  depending on the ticks for motor with gear ratio of 18:1 and wheel of 4.125" diameter
+**/
+double convertInchesIntoTicks_18(float inches) {
     // Convert inches into ticks (based on gear ratio)
-    //    - 900 ticks for 18:1 gear ratio
-    //    - 1,800 ticks for 36:1 gear ratio
-    int diameter_of_wheel = 4;
-    double ticks = 900*inches/(diameter_of_wheel*3.14);
+    // 900 ticks per revolution for 18:1 gear ratio
+    double ticks = TICKS_PER_REVOLUTION_18*inches/WHEEL_CIRCUMFERENCE;
     return ticks;
 }
 
-int convertDegreesIntoTicks(float degrees) {
+/**
+  convertInchesIntoTicks_36 converts 'inches' into the appropriate number of motor ticks
+  depending on the ticks for motor with gear ratio of 36:1 and wheel of 4.125" diameter
+**/
+double convertInchesIntoTicks_36(float inches) {
     // Convert inches into ticks (based on gear ratio)
-    //    - 900 ticks for 18:1 gear ratio
-    //    - 1,800 ticks for 36:1 gear ratio
-    double ticks = 900*degrees/360;
+    // 1,800 ticks per revolution for 18:1 gear ratio
+    double ticks = TICKS_PER_REVOLUTION_36*inches/WHEEL_CIRCUMFERENCE;
+    return ticks;
+}
+
+/**
+  convertDegreesIntoTicks_18 converts 'degrees' into the appropriate number of motor ticks
+  depending on the ticks for motor with gear ratio of 18:1 and wheel of 4.125" diameter
+**/
+int convertDegreesIntoTicks_18(float degrees) {
+    // Convert inches into ticks (based on gear ratio)
+    // 900 ticks per revolution for 18:1 gear ratio
+    double ticks = TICKS_PER_REVOLUTION_18*degrees/360;
     return ticks;  
 }
 
+/**
+  convertDegreesIntoTicks_36 converts 'degrees' into the appropriate number of motor ticks
+  depending on the ticks for motor with gear ratio of 36:1 and wheel of 4.125" diameter
+**/
+int convertDegreesIntoTicks_36(float degrees) {
+    // Convert inches into ticks (based on gear ratio)
+    // 1,800 ticks per revolution for 18:1 gear ratio
+    double ticks = TICKS_PER_REVOLUTION_36*degrees/360;
+    return ticks;  
+}
+
+/** 
+   moveArms moves the arms up and down by 'degrees' and 
+   at a certain 'speed' pct. Use positive number in degrees
+   to move up and negative number to move down. ArmMotor uses
+   a cartrigde with 36:1 gear ratio.
+**/
 void moveArms(float degrees, int speed)
 {
     // 1. Convert inches into ticks
-    double ticks = convertDegreesIntoTicks(degrees);
+    double ticks = convertDegreesIntoTicks_36(degrees);
 
-    // 2. Set the initial motor encoder counters to 0
+    // 2. Set the initial motor encoder counter to 0
     ArmMotor.setRotation(0, vex::rotationUnits::raw);
   
     // 3. Set the velocity of the motor to 'speed'
@@ -69,7 +108,7 @@ void moveArms(float degrees, int speed)
     // 5. Loop - while (counter variable < ticks)
     while (ticksAM < ticks) {
       
-          if (ticksAM < ticks) {
+          if (ticksAM < ticks && !ArmMotor.isSpinning()) {
             double moveAMTicks = 0;
 
             // read the counter value
@@ -93,10 +132,63 @@ void moveArms(float degrees, int speed)
     // We are done moving the arms  
 }
 
+/** 
+   moveStacker moves the stacker up and down by 'degrees' and 
+   at a certain 'speed' pct. Use positive number in degrees
+   to move up and negative number to move down. StackerMotor uses
+   a cartrigde with 36:1 gear ratio.
+**/
+void moveStacker(float degrees, int speed)
+{
+    // 1. Convert inches into ticks
+    double ticks = convertDegreesIntoTicks_36(degrees);
+
+    // 2. Set the initial motor encoder counters to 0
+    StackerMotor.setRotation(0, vex::rotationUnits::raw);
+  
+    // 3. Set the velocity of the motor to 'speed'
+    StackerMotor.setVelocity(speed, velocityUnits::pct);
+
+    // 4. Create counter variable and set it to 0
+    double ticksSM = 0;
+
+    // 5. Loop - while (counter variable < ticks)
+    while (ticksSM < ticks && !StackerMotor.isSpinning()) {
+      
+          if (ticksSM < ticks) {
+            double moveSMTicks = 0;
+
+            // read the counter value
+            ticksSM = StackerMotor.rotation(vex::rotationUnits::raw);
+
+            // calculate remainingTicks to move
+            double remainingTicks = ticks - ticksSM;
+
+            // if remainingTicks > 50, set moveTicks (variable) = 50
+            // else set moveTicks = remainingTicks
+            if (remainingTicks > 50) {
+              moveSMTicks = 50;
+            } else {
+              moveSMTicks = remainingTicks;
+            }
+
+            // rotateFor(moveTicks, raw, false)
+            ArmMotor.rotateFor(moveSMTicks, vex::rotationUnits::raw, false);
+          }
+    }
+    // We are done moving the stacker  
+}
+
+/** 
+   moveClaws spins both the claws in and out by 'degrees' and 
+   at a certain 'speed' pct. Use positive number in degrees
+   to move in and negative number to move out. Claw motors use
+   a cartrigde with 18:1 gear ratio.
+**/
 void moveClaws(float degrees, int speed) 
 {
     // 1. Convert inches into ticks
-    double ticks = convertDegreesIntoTicks(degrees);
+    double ticks = convertDegreesIntoTicks_18(degrees);
 
     // 2. Set the initial motor encoder counters to 0
     LeftClawMotor.setRotation(0, vex::rotationUnits::raw);
@@ -111,8 +203,9 @@ void moveClaws(float degrees, int speed)
     double ticksRCM = 0;
 
     // 5. Loop - while (any counter variable < ticks)
-    while (ticksLCM < ticks || ticksRCM < ticks) {   
-          if (ticksLCM < ticks) {
+    while (ticksLCM < ticks || ticksRCM < ticks) { 
+
+          if (ticksLCM < ticks && !LeftClawMotor.isSpinning()) {
             double moveLCMTicks = 0;
 
             // read the counter value
@@ -133,7 +226,7 @@ void moveClaws(float degrees, int speed)
             LeftClawMotor.rotateFor(moveLCMTicks, vex::rotationUnits::raw, false);
           }
 
-          if (ticksRCM < ticks) {
+          if (ticksRCM < ticks && !RightClawMotor.isSpinning()) {
             double moveRCMTicks = 0;
 
             // read the counter value
@@ -157,10 +250,16 @@ void moveClaws(float degrees, int speed)
     // We are done moving the claws
 }
 
+/** 
+   moveRobot moves all 4 base motors forward and backward by 'inches' 
+   and at a certain 'speed' pct. Use positive number in inches to 
+   move forward and negative number to move backward. Base motors use
+   a cartrigde with 18:1 gear ratio.
+**/
 void moveRobot(float inches, int speed) 
 {
     // 1. Convert inches into ticks
-    double ticks = convertInchesIntoTicks(inches);
+    double ticks = convertInchesIntoTicks_18(inches);
 
     // 2. Set the initial motor encoder counters to 0
     LeftFrontMotor.setRotation(0, vex::rotationUnits::raw);
@@ -184,7 +283,7 @@ void moveRobot(float inches, int speed)
     while (ticksLFM < ticks || ticksRFM < ticks || 
            ticksLBM < ticks || ticksRBM < ticks) {
       
-          if (ticksLFM < ticks) {
+          if (ticksLFM < ticks && !LeftFrontMotor.isSpinning()) {
             double moveLFMTicks = 0;
 
             // read the counter value
@@ -205,7 +304,7 @@ void moveRobot(float inches, int speed)
             LeftFrontMotor.rotateFor(moveLFMTicks, vex::rotationUnits::raw, false);
           }
 
-          if (ticksLBM < ticks) {
+          if (ticksLBM < ticks && !LeftBackMotor.isSpinning()) {
             double moveLBMTicks = 0;
 
             // read the counter value
@@ -226,7 +325,7 @@ void moveRobot(float inches, int speed)
             LeftBackMotor.rotateFor(moveLBMTicks, vex::rotationUnits::raw, false);
           }
 
-          if (ticksRBM < ticks) {
+          if (ticksRBM < ticks && !RightBackMotor.isSpinning()) {
             double moveRBMTicks = 0;
 
             // read the counter value
@@ -247,7 +346,7 @@ void moveRobot(float inches, int speed)
             RightBackMotor.rotateFor(moveRBMTicks, vex::rotationUnits::raw, false);
           }   
 
-          if (ticksRFM < ticks) {
+          if (ticksRFM < ticks && !RightFrontMotor.isSpinning()) {
             double moveRFMTicks = 0;
 
             // read the counter value
@@ -272,15 +371,100 @@ void moveRobot(float inches, int speed)
     // We are done moving the robot
 }
 
-void setStartingPosition() {
-  ArmMotor.setVelocity(100, velocityUnits::pct);  
+/** 
+   startSpinningClaws starts spinning the claw motors either for intake or
+   to push the cubes out at a certain 'speed' pct. Set the 'direction' = 1 
+   to move cubes in and set 'direction' = 2 to move the cubes out. If the
+   claws are already spinning, it will do nothing
+**/
+void startSpinningClaws(int direction, int speed)
+{
+  // 1. Set the speed for both the claws
+  if (!LeftClawMotor.isSpinning()) {
+    LeftClawMotor.setVelocity(speed, vex::velocityUnits::pct);
+  }
+  if (!RightClawMotor.isSpinning()) {
+    RightClawMotor.setVelocity(speed, vex::velocityUnits::pct);
+  }
+
+  // 2. Now begin spinning the claws
+  if (!LeftClawMotor.isSpinning() && direction == 1) {
+    LeftClawMotor.spin(forward);
+  } else if (!LeftClawMotor.isSpinning() && direction == 2) {
+    LeftClawMotor.spin(reverse);
+  }
+
+  if (!RightClawMotor.isSpinning() && direction == 1) {
+    RightClawMotor.spin(reverse);
+  } else if (!RightClawMotor.isSpinning() && direction == 2) {
+    RightClawMotor.spin(forward);
+  }
+
+  // We are done starting to spin the claw motors at the 'speed'
+}
+
+/** 
+   stopSpinningClaws stop spinning the claw motors. If the
+   claws are not spinning, it will do nothing
+**/
+void stopSpinningClaws()
+{
+  // Stop spinning the claws
+  if (LeftClawMotor.isSpinning()) {
+    LeftClawMotor.stop();
+  }
+
+  if (RightClawMotor.isSpinning()) {
+    RightClawMotor.stop();
+  }
+
+  // We are done stopping to spin the claw motors
+}
+
+/**
+  turnRobot turns the robot by 'degrees'. It does so by turning its front two motors at a certain 'speed'. 
+  In order to move left (negative degrees), it moves the left front wheel back by degrees/2 and right 
+  front wheel forward by degrees/2. To move right (positive degrees), it moves the left front wheel forward 
+  by degrees/2 and right front wheel back by degrees/2. Note: +90 degrees is a right turn
+**/
+void turnRobot(float degrees, int speed) 
+{
+    // A four-wheeled robot has a turning diameter approximated by the diagonal distance 
+    // between the wheels. This distance can simply be measured on the robot as the distance 
+    // between the upper left and lower right wheels at the points where they contact the
+    // ground  
+    float turningRatio = TURNING_DIAMETER / WHEEL_DIAMETER;
+    float wheelDegrees = turningRatio * degrees; 
+
+    // Divide by two because each wheel provides half the rotation
+    LeftFrontMotor.startRotateFor(wheelDegrees * 1/2, vex::rotationUnits::deg, speed, vex::velocityUnits::pct);
+    RightFrontMotor.rotateFor(wheelDegrees * 1/2, vex::rotationUnits::deg, speed, vex::velocityUnits::pct);
+}
+
+/**
+  setStartingPosition prepares the robot to move into the starting 
+  position after the timer is started
+**/ 
+void setStartingPosition() 
+{
+  // 1. Push the stacker out
   StackerMotor.rotateFor(200, vex::rotationUnits::deg, false);
+
+  // 2. Even while the stacker is moving out, raise the arms
+  ArmMotor.setVelocity(100, velocityUnits::pct); 
   ArmMotor.rotateFor(400, vex::rotationUnits::deg);
+
+  // 3. Spin the claws outward to untangle the stacker if it gets stuck
   RightClawMotor.rotateFor(180, vex::rotationUnits::deg, false);
   LeftClawMotor.rotateFor(180, vex::rotationUnits::deg, true);
+
+  // 4. Bring the arms back to the starting position
   ArmMotor.rotateFor(-400, vex::rotationUnits::deg);
+
+  // 5. Bring the stacker back to the starting position
   StackerMotor.rotateFor(-200, vex::rotationUnits::deg, false);
 }
+
 
 int main() {
   vexcodeInit();
@@ -291,12 +475,26 @@ int main() {
   // Move the robot 36" at 75% speed
   moveRobot(36, 75); 
 
-  // Move the arm motor 180 degrees at 50% speed
-  moveArms(180, 50);
+  // Move the arm motor 30 degrees at 50% speed
+  moveArms(30, 50);
 
   // Move the claws 45 degrees at 80% speed
   moveClaws(45, 80);
   
+  // Move the stacker 30 degrees at 90% speed
+  moveStacker(30, 90);
+
+  // Start spinning the claws for intake (1) at 80% speed
+  startSpinningClaws(1, 80);
+
+  wait(5, seconds);
+
+  // Stop spinning the claws
+  stopSpinningClaws();
+
+  // Turn the robot right by 45 degrees at 25% speed
+  turnRobot(45, 25);
+
 }
 
 
