4a94749ba6eb72bc364c87c373685a9d7e1ac2bf
Riya Kadakia <riyakadakia@riyas-air.lan>
2020-03-06 09:15:50 -0800

    state 4 point red version

---
+++ state/Team_4_Red/include/robot-config.h
@@ -0,0 +1,24 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern motor ArmMotor;
+extern motor StackerMotor;
+extern controller Controller1;
+extern motor LeftClawMotor;
+extern motor RightClawMotor;
+extern inertial GyroSensor;
+extern motor LeftFrontMotor;
+extern motor RightFrontMotor;
+extern motor LeftBackMotor;
+extern motor RightBackMotor;
+extern bumper BumperA;
+extern bumper BumperH;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
