3930e279ea3770fb21cabb3afe84b3bf6948e795
Riya Kadakia <riyakadakia@riyas-air.lan>
2020-03-06 16:17:04 -0800

    4 point autonomous program for blue side

---
+++ state/Team_4_Blue/src/robot-config.cpp
@@ -0,0 +1,35 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor ArmMotor = motor(PORT6, ratio36_1, true);
+motor StackerMotor = motor(PORT7, ratio36_1, false);
+controller Controller1 = controller(primary);
+motor LeftClawMotor = motor(PORT4, ratio36_1, false);
+motor RightClawMotor = motor(PORT3, ratio36_1, true);
+inertial GyroSensor = inertial(PORT15);
+motor LeftFrontMotor = motor(PORT10, ratio18_1, false);
+motor RightFrontMotor = motor(PORT9, ratio18_1, true);
+motor LeftBackMotor = motor(PORT8, ratio18_1, false);
+motor RightBackMotor = motor(PORT1, ratio18_1, true);
+bumper BumperA = bumper(Brain.ThreeWirePort.A);
+bumper BumperH = bumper(Brain.ThreeWirePort.H);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
